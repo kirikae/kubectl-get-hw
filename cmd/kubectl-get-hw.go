3a687fe96b5caed549784b60b06e39023f872d95
Ben C-S <22813189+kirikae@users.noreply.github.com>
2025-09-17 15:30:45 +1000

    Initial commit

---
+++ cmd/kubectl-get-hw.go
@@ -0,0 +1,12 @@
+package main
+import (
+    "fmt"
+    "os"
+    "path/filepath"
+    "k8s.io/client-go"
+	"k8s.io/cli-runtime"
+)
+
+func main (
+
+)
---
+++ go.mod
@@ -0,0 +1,8 @@
+module github.com/kirikae/kubectl-get-hw
+
+go 1.24.0
+
+require (
+	k8s.io/cli-runtime v0.34.1
+	k8s.io/client-go v0.34.1
+)
